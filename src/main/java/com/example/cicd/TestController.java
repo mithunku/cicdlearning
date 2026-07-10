3abd2ae29822890c945766b56abe02141144fc33
Mithun Kumar <mithunph2392001@gmail.com>
2026-07-10 10:37:29 +0530

    added three controller

---
+++ src/main/java/com/example/cicd/TestController.java
@@ -0,0 +1,27 @@
+package com.example.cicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test()
+    {
+        return "hello world";
+    }
+
+    @GetMapping("/test2")
+    public String test2()
+    {
+        return "test hello world";
+    }
+
+    @GetMapping("/test3")
+    public String test3()
+    {
+        return "test hello world";
+    }
+
+}
